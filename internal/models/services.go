cae43b9b907b8915ecf6e6f6aa71ae52c1dc6fec
root <nameerrror3301@gmail.com>
2022-12-18 17:50:01 +0200

    Реализовал добавление данных в нужную таблицу DB

--- internal/models/services.go
+++ internal/models/services.go
@@ -1,6 +1,10 @@
 package models
 
 import (
+	"GoStorageService/internal/database"
+	"database/sql"
+	"fmt"
+
 	gojson "github.com/goccy/go-json"
 	"github.com/sirupsen/logrus"
 )
@@ -44,14 +48,43 @@ func AddingData(payload []byte, market string, category string) (bool, string) {
 		return false, "Incorrect content data"
 	}
 
-	/*
-		Добовление данных в базу отталкиваясь от Query параметров (СПРОЕКТИРОВАТЬ БАЗУ)
-	*/
+	db, err := database.ConnectDataBase()
+	if err != nil {
+		return false, "Storage is not connected"
+	}
+	defer db.Close()
 
-	// out, _ := gojson.Marshal(&data)
-	// fmt.Println(string(out))
+	RemoveData(db, data, market, category)
+
+	if status, massage := AddData(db, data, market, category); !status {
+		return status, massage
+	}
 
 	return true, "Success Adding"
+}
+
+// Реализованна для того чтобы пока нет реализации по обновлению данных в базе не получать дублирование результатов
+func RemoveData(db *sql.DB, data []Data, market string, category string) {
+	query := fmt.Sprintf("DELETE FROM %s_%s", market, category)
+
+	if _, err := db.Exec(query); err != nil {
+		logrus.Infof("Err remove data in %s_%s - %s", market, category, err)
+	}
+}
+
+// Добавление новых данных
+func AddData(db *sql.DB, data []Data, market string, category string) (bool, string) {
+	query := fmt.Sprintf(
+		"INSERT INTO %s_%s (user_name, registration_date, phone_number, product_name, product_photo_url, product_price, product_description, product_url) VALUES($1, $2, $3, $4, $5, $6, $7, $8)", market, category)
+	for _, val := range data {
+		// Перед добавлением реализовать обновление уже имеющихся данных
+		if _, err := db.Exec(query, val.User.Name, val.User.DateRegistr, val.User.PhoneNumber, val.Products.ProdName, val.Products.PhotoUrl, val.Products.Price, val.Products.Description, val.Products.ProdName); err != nil {
+			logrus.Errorf("Err addings data to table - %s", err)
+			return false, "Err add data"
+		}
+	}
+	logrus.Infof("The %s_%s table has been updated with the following data", market, category)
+	return true, ""
 }
 
 // Проверка параметров URL
