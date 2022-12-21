9c529aeaf7e4e0fb0cd786d425b017d455dfb981
root <nameerrror3301@gmail.com>
2022-12-21 14:31:31 +0200

    Wallapop неисправен и был отложен

--- migration/000001_init.up.sql
+++ migration/000001_init.up.sql
@@ -1,10 +1,3 @@
-CREATE TABLE users_telegram
-(
-    id serial not null unique,
-    username varchar(255) not null,
-    pass varchar(255) not null,
-    subscription boolean
-);
 
 CREATE TABLE sbazar_electronics
 (
@@ -71,70 +64,7 @@ CREATE TABLE sbazar_babymom
     product_url varchar
 );
 
-CREATE TABLE wallapop_electronics
-(
-    id serial not null unique,
-    user_name varchar(255),
-    registration_date varchar(255),
-    phone_number varchar(25),
-    product_name varchar(255),
-    product_photo_url varchar(300),
-    product_price varchar(80),
-    product_description varchar,
-    product_url varchar
-);
 
-CREATE TABLE wallapop_cars
-(
-    id serial not null unique,
-    user_name varchar(255),
-    registration_date varchar(255),
-    phone_number varchar(25),
-    product_name varchar(255),
-    product_photo_url varchar(300),
-    product_price varchar(80),
-    product_description varchar,
-    product_url varchar
-);
-
-CREATE TABLE wallapop_phototv
-(
-    id serial not null unique,
-    user_name varchar(255),
-    registration_date varchar(255),
-    phone_number varchar(25),
-    product_name varchar(255),
-    product_photo_url varchar(300),
-    product_price varchar(80),
-    product_description varchar,
-    product_url varchar
-);
-
-CREATE TABLE wallapop_home
-(
-    id serial not null unique,
-    user_name varchar(255),
-    registration_date varchar(255),
-    phone_number varchar(25),
-    product_name varchar(255),
-    product_photo_url varchar(300),
-    product_price varchar(80),
-    product_description varchar,
-    product_url varchar
-);
-
-CREATE TABLE wallapop_motos
-(
-    id serial not null unique,
-    user_name varchar(255),
-    registration_date varchar(255),
-    phone_number varchar(25),
-    product_name varchar(255),
-    product_photo_url varchar(300),
-    product_price varchar(80),
-    product_description varchar,
-    product_url varchar
-);
 
 
 
